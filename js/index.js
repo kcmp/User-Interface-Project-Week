07cab37adcd93f0008008b43aa676789d2c69a52
unknown <kcmp711@gmail.com>
2019-02-14 22:39:49 -0500

    updated index.js

--- js/index.js
+++ js/index.js
@@ -1,32 +1,26 @@
 // JS goes here
 window.addEventListener('load', function () {
-    icon = document.querySelector(".icon");
-    body = document.querySelector("body");
-    showDiv = document.querySelector(".show-div");
-    container = document.querySelector(".container");
-    close = document.querySelector(".close");
-    icon.addEventListener("click", function () {
-    icon.style.display = "none";
-    showDiv.style.display = "block";
-    container.style.display = "none";
-     })
-  
-     close = document.querySelector(".close");
-     close.addEventListener("click", function () {
-     icon.style.display = "block";
-     showDiv.style.display = "none";
-     container.style.display = "block";
-  
-     })  
+  icon = document.querySelector(".icon");
+  body = document.querySelector("body");
+  showDiv = document.querySelector(".show-div");
+  container = document.querySelector(".container");
+  close = document.querySelector(".close");
+  icon.addEventListener("click", function () {
+  icon.style.display = "none";
+  showDiv.style.display = "block";
+  container.style.display = "none";
    })
 
+    close = document.querySelector(".close");
+   close.addEventListener("click", function () {
+   icon.style.display = "block";
+   showDiv.style.display = "none";
+   container.style.display = "block";
+
+    })
+
+
+  }) 
+
+
    //highlight feature for navbar
-const link = document.querySelectorAll('nav a');
-for(i=0;i<link.length;i++){
-	link[i].addEventListener('mouseenter',function(event){
-		event.target.style.color = teal;
-	})
-	link[i].addEventListener('mouseout', function(event){
-		event.target.style.color = white;
-	})
-}
