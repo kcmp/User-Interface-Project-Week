a1b9e359658b16bcf232db99d6d5e82df1d0cbe8
unknown <kcmp711@gmail.com>
2019-02-16 11:43:41 -0500

    new configuring for navbar, updated mixins.less, added to index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,44 @@
 // JS goes here
-window.addEventListener('load', function () {
-  icon = document.querySelector(".icon");
-  body = document.querySelector("body");
-  showDiv = document.querySelector(".show-div");
-  container = document.querySelector(".container");
-  close = document.querySelector(".close");
-  icon.addEventListener("click", function () {
-  icon.style.display = "none";
-  showDiv.style.display = "block";
-  container.style.display = "none";
-   })
 
-  close = document.querySelector(".close");
-  close.addEventListener("click", function () {
-  icon.style.display = "block";
-  showDiv.style.display = "none";
-  container.style.display = "block";
+//The code below works on live server but does not work in githb html preview
+// window.addEventListener('load', function () {
+//   icon = document.querySelector(".icon");
+//   body = document.querySelector("body");
+//   showDiv = document.querySelector(".show-div");
+//   container = document.querySelector(".container");
+//   close = document.querySelector(".close");
+//   icon.addEventListener("click", function () {
+//   icon.style.display = "none";
+//   showDiv.style.display = "block";
+//   container.style.display = "none";
+//    })
 
-    })
+//   close = document.querySelector(".close");
+//   close.addEventListener("click", function () {
+//   icon.style.display = "block";
+//   showDiv.style.display = "none";
+//   container.style.display = "block";
+
+//     })
 
 
-  }) 
+//   }) 
 
+class ToggleMenu {
+  constructor(domElement) { 
+      this.domElement = domElement; 
+      this.toggleButton = this.domElement.querySelector('.menu-nav'); 
 
-   //highlight feature for navbar
+       this.toggleButton.addEventListener('click', this.expandNav);
+
+   }
+
+   expandNav() {
+      const navs = document.querySelectorAll('.nav');
+
+       Array.from(navs).forEach(element => element.classList.toggle('show'));
+  }
+
+}
+
+const navs = document.querySelectorAll('.nav').forEach( navs => new ToggleMenu(navs));
